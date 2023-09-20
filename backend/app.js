fea56776ceb5a611142f566eed690f624c34e731
Aryaman Kumar Sharma <kumararyaman26@gmail.com>
2023-09-21 00:48:20 +0530

    Minor Bug Fix

--- backend/app.js
+++ backend/app.js
@@ -2,15 +2,17 @@ import express from "express";
 import mongoose from "mongoose";
 import router from "./routes/user-routes";
 import blogRouter from "./routes/blog-routes";
+import dotenv from 'dotenv';
 
 const app = express();
 
+dotenv.config();
 app.use(express.json());
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
 mongoose
   .connect(
-    "mongodb+srv://admin:V32JicAGbAuJSIxL@cluster0.0wvpisn.mongodb.net/Blog?retryWrites=true&w=majority"
+    process.env.DB_CONNECT,
   )
   .then(() => app.listen(5000))
   .then(() =>
